7490cdceb7a3df35d9e0e506b0a2bd224a9fe228
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-05 21:50:18 +0530

    reversing string using (stack)

--- stack/reversing a string using stack/reverse_string.c
+++ stack/reversing a string using stack/reverse_string.c
@@ -1,1 +1,29 @@
+#include"stdio.h"
+#define SIZE 10000
 
+char arr[SIZE];
+int top =-1;
+
+void main()
+{
+    char str[10000];
+    printf("Input a string\n");
+    scanf("%[^\n]",str);
+    reverse_stack(str);
+    printf("The reversed string using stack is:\n%s",str);
+}
+
+void reverse_stack(char str[])
+{
+    int i;
+    for(i = 0;str[i];i++)
+    {
+        arr[++top] = str[i];
+    }
+    i = -1;
+    while (top >= 0)
+    {
+        str[++i] = arr[top--];
+
+    }
+}
