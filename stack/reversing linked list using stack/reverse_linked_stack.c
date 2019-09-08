746cca522d2a41a104c2dfa9e54fd7dda8c842db
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-08 11:40:35 +0530

    linked list reversing using stack

--- stack/reversing linked list using stack/reverse_linked_stack.c
+++ stack/reversing linked list using stack/reverse_linked_stack.c
@@ -1,6 +1,10 @@
 
 #include"stdio.h"
 #define MAX 1000
+#include"conio.h"
+#include"stdlib.h"
+
+
 
 typedef struct
 {
@@ -9,15 +13,20 @@ typedef struct
 }Node;
 
 Node *head = NULL;
-Node stack[MAX];
+Node *stack[MAX];
+
+
 
 void main()
 {
     int n;
     while(1)
     {
-        printf("What do you want to do?\n1. Insert\n2. Print\n3. Reverse\n4. Exit\n");
+        fflush(stdin);
+        system("clear");
+        printf("\nWhat do you want to do?\n1. Insert\n2. Print\n3. Reverse\n4. Exit\n");
         scanf("%d",&n);
+        fflush(stdin);
         switch(n)
         {
         case 1:
@@ -38,7 +47,7 @@ void main()
         default: printf("Invalid selection\n");
 
         }
-        _getch();
+        getch();
     }
 }
 
@@ -47,15 +56,18 @@ void insert()
 {
     int num;
     Node *temp = (Node *)malloc(sizeof(Node));
-    temp->next = NULL
+    temp->next = NULL;
+    fflush(stdin);
     printf("Enter a number\n");
     scanf("%d",&temp->data);
+    fflush(stdin);
     if(head == NULL)
     {
+        stack[0] = NULL;
         head = temp;
         return;
     }
-    temp1 = head;
+    Node *temp1 = head;
     while(temp1->next != NULL)
     {
         temp1 = temp1->next;
@@ -66,28 +78,39 @@ void insert()
 
 void reverse()
 {
-    int i;
-    Node *prev=NULL;
-    Node *temp = head;
+    int top = 0;
+    Node *temp1 = head;
+    while(temp1)
+    {
+        stack[++top] = temp1;
+        temp1 = temp1->next;
+    }
+
+    Node *temp = stack[top--];
+    head = temp;
     while(temp)
     {
-        stack[i++]->next=temp;
-        prev = temp;
+        temp->next = stack[top--];
         temp = temp->next;
     }
-    head->next = NULL;
-    head = prev;
-
-    while(temp)
-    {
-
-    }
-
-
 
 
 }
 
+void print()
+{
+    Node *temp = head;
+    if (head == NULL)
+    {
+        printf("List Empty\n");
+        return;
+    }
 
+    while(temp)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+}
 
 
